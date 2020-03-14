d258f21f51dfebda552359aca1c71130c8491f40
Abraham E. Tavarez <abetavarez@icloud.com>
2020-03-14 18:15:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
 console.log('ready')
 
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+const cards = ['queen', 'queen', 'king', 'king'];
 
-console.log('user flipped ' + cardFour)
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log(`User flipped ${cardOne}`)
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log(`User flipped ${cardTwo}`);
+
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert('You found a match');
+    } 
+    else {
+        alert('Sorry. try again');
+    }
+}
