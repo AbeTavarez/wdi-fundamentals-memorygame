c14623c3e082c1fac2f13bdb3fdc5f76156654bb
Abraham E. Tavarez <abetavarez@icloud.com>
2020-03-14 22:16:28 -0400

    created game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,25 +2,28 @@ console.log('ready')
 
 const cards = ['queen', 'queen', 'king', 'king'];
 
+//store cards flipped by the user
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log(`User flipped ${cardOne}`)
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log(`User flipped ${cardTwo}`);
-
-if (cardsInPlay.length === 2){
-    if (cardsInPlay[0] === cardsInPlay[1]){
-        alert('You found a match');
-    } 
-    else {
-        alert('Sorry. try again');
-    }
+//func will check if for a match
+const checkForMatch = function(){
+    if (cardsInPlay.length === 2){
+        if (cardsInPlay[0] === cardsInPlay[1]){
+            alert('You found a match');
+        } 
+        else {
+            console.log('Sorry. try again');
+        }
+    };
 }
+
+
+//push cards flipped by user and calls checkForMatch();
+const flipCard = function(cardID){
+console.log(`User flipped ${cards[cardID]}`);
+cardsInPlay.push(cards[cardID]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
