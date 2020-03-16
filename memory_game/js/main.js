e7abca8617307a0da4f9cdbbe4949a20bd476d06
Abraham E. Tavarez <abetavarez@icloud.com>
2020-03-16 00:43:31 -0400

    added events listeners and rest button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,22 +33,56 @@ const checkForMatch = function(){
             alert('You found a match');
         } 
         else {
-            console.log('<--- Sorry. try again --->');
-        }
-    };
-}
+            alert('<--- Sorry. try again --->');
+        };
+    }
+       
+};
 
 
 //push cards flipped by user and calls checkForMatch();
-const flipCard = function(cardID){
-console.log(`User flipped ${cards[cardID].rank}`);
-console.log(cards[cardID].cardImage);
-console.log(cards[cardID].suit);
-cardsInPlay.push(cards[cardID].rank);
-checkForMatch();
+const flipCard = function(){
+    let cardID = this.getAttribute('data-id');
+
+    console.log(cardID);
+    console.log(`User flipped ${cards[cardID].rank}`);
+    console.log(cards[cardID].cardImage);
+    console.log(cards[cardID].suit);
+
+    cardsInPlay.push(cards[cardID].rank);
+    this.setAttribute('src', cards[cardID].cardImage);
+
+    //set time out for card to fully flip
+   setTimeout(checkForMatch, 100);
 };
 
-flipCard(0);
-flipCard(2);
 
+const createBoard = function(){
+    for (let i = 0; i < cards.length; i++){
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        let gameBoard = document.getElementById('game-board');
+        gameBoard.appendChild(cardElement);
+    }
+};
 
+//Reset game function
+const resetGame = function(){
+    location.reload();
+}
+
+//reset Button
+const resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', resetGame);
+
+//User Score
+    //display user current score
+
+    //update user score
+
+//Shuffle Cards
+    //
+
+createBoard();
